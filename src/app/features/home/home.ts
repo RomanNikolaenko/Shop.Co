7831fdf9646f8f46a01fd8e0d6fe4a8271151d6a
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2025-08-04 16:45:43 +0300

    added section hero

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { Hero } from './components/hero/hero';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
