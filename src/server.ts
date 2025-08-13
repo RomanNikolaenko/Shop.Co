c4afe99bb734eec5bb6db1650e763d68f482550d
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2025-08-13 16:58:51 +0300

    edit

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,75 @@
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
+import { join } from 'node:path';
 
-const angularAppEngine = new AngularAppEngine();
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
+import express, { static as expressStatic } from 'express';
 
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
+const browserDistFolder = join(import.meta.dirname, '../browser');
 
-  // Example API endpoints can be defined here.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
+const app = express();
+const angularApp = new AngularNodeAppEngine();
 
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response('Not found', { status: 404 });
+/**
+ * Example Express Rest API endpoints can be defined here.
+ * Uncomment and define endpoints as necessary.
+ *
+ * Example:
+ * ```ts
+ * app.get('/api/{*splat}', (req, res) => {
+ *   // Handle API request
+ * });
+ * ```
+ */
+
+/**
+ * Serve static files from /browser
+ */
+app.use(
+  expressStatic(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false,
+  }),
+);
+
+/**
+ * Handle all other requests by rendering the Angular application.
+ * Переписано на async/await для коректної роботи з promise/no-callback-in-promise
+ */
+app.use(async(req, res, next) => {
+  try {
+    const response = await angularApp.handle(req);
+    if (response) {
+      writeResponseToNodeResponse(response, res);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * Start the server if this module is the main entry point.
+ * The server listens on the port defined by the `PORT` environment variable,
+ * or defaults to 4000.
+ */
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORTSERVER'] || 4000;
+  app.listen(port, (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`Node Express server listen on http://localhost:${port}`);
+  });
 }
 
 /**
- * The request handler used by the Angular CLI (dev-server and during build).
+ * Request handler used by the  ngular CL I (for dev-server and during build)
+ * or Firebase Cloud Functions.
  */
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
+export const reqHandler = createNodeRequestHandler(app);
