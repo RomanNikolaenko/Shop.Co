08adba56ff7a571896c54b568b8df44e4df485f9
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2025-08-15 09:12:48 +0300

    sdf

--- src/server.ts
+++ src/server.ts
@@ -1,75 +1,14 @@
-import { join } from 'node:path';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+import { getContext } from '@netlify/angular-runtime/context.mjs';
 
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express, { static as expressStatic } from 'express';
+const angularAppEngine = new AngularAppEngine();
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+  const context = getContext();
 
-const app = express();
-const angularApp = new AngularNodeAppEngine();
+  const result = await angularAppEngine.handle(request, context);
 
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
-/**
- * Serve static files from /browser
- */
-app.use(
-  expressStatic(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
-
-/**
- * Handle all other requests by rendering the Angular application.
- * Переписано на async/await для коректної роботи з promise/no-callback-in-promise
- */
-app.use(async (req, res, next) => {
-  try {
-    const response = await angularApp.handle(req);
-    if (response) {
-      writeResponseToNodeResponse(response, res);
-    } else {
-      next();
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-/**
- * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable,
- * or defaults to 4000.
- */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORTSERVER'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
-    console.log(`Node Express server listen on http://localhost:${port}`);
-  });
+  return result || new Response('Not found', { status: 404 });
 }
 
-/**
- * Request handler used by the  ngular CL I (for dev-server and during build)
- * or Firebase Cloud Functions.
- */
-export const reqHandler = createNodeRequestHandler(app);
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
